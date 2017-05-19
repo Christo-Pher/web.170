269b4a33ee78b61d19195b4c1bc9ffe3ec7ca3c7
Christopher Cornwell <Christo-Pher@users.noreply.github.com>
2017-05-19 09:46:27 -0700

    cleaned up blog loop

--- index.php
+++ index.php
@@ -9,12 +9,7 @@
     <h4><a href="<?php the_permalink(); ?>"><?php the_title(''); ?></a></h4>
     <small>Posted on <?php the_time('F jS, Y') ?> in <?php the_category(', ') ?> by <?php the_author(); ?></small><br>
     <div class="thumbnail-excerpt-container">
-    <a href="<?php the_permalink(); ?>"><?php if (class_exists('MultiPostThumbnails')) :
-    MultiPostThumbnails::the_post_thumbnail(
-        get_post_type(),
-        'secondary-image'
-    );
-endif; ?></a>
+    <a href="<?php the_permalink(); ?>"><?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail( get_post_type(), 'secondary-image' ); endif; ?></a>
     <p><?php echo get_the_excerpt(); ?><a href="<?php the_permalink(); ?>">....READ MORE</a></p>
     </div>
     <?php endwhile; endif ; ?>
